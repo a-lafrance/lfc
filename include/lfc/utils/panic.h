d7fade2b68cda9bf17a79234d1dd3497b9d1ba4d
Arthur Lafrance <lafrancearthur@gmail.com>
2021-11-14 22:42:53 -0800

    impl'd guard & unwrap macros

--- include/lfc/utils/panic.h
+++ include/lfc/utils/panic.h
@@ -8,8 +8,11 @@
 /// way to print an error message to the user before exiting. It prints a message to stderr, along with
 /// some basic debug data that specifies where the error occurred.
 
-#define panic(status, fmt_str, ...) fprintf(stderr, "[%s:%d @ %s] program panicked due to error:\n\t" fmt_str "\n", \
+#define panic(estatus, fmt_str, ...) fprintf(stderr, "[%s:%d @ %s] program panicked due to error:\n\t" fmt_str "\n", \
                                             __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
-                                    exit(status);
+                                    exit(estatus);
+
+#define guard(estatus, condition) if (!(condition)) { panic(estatus, "guard failed: %s", #condition) } // "panic if false"
+#define unwrap(estatus, ptr) if (ptr == NULL) { panic(estatus, "failed to unwrap '%s'", #ptr) }
 
 #endif
